12eccc8e7e187f081f0a221b16fbe11e97d3e410
nickcatalin <florin.nicola@stud.acs.upb.ro>
2025-07-20 19:56:16 +0300

    Initialize Next.js project with UI components
    Set up a new Next.js project with TypeScript, ESLint, Tailwind CSS, and shadcn/ui components. Added initial configuration files, public assets, utility libraries, and updated the README with setup and usage instructions. Updated .gitignore for better environment and dependency management.

---
+++ src/lib/appwrite.ts
@@ -0,0 +1,13 @@
+import { Account, Avatars, Client, Databases, Storage } from 'appwrite';
+
+const client = new Client();
+
+client
+    .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT || '')
+    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID || '');
+
+export { client };
+export const account = new Account(client);
+export const avatars = new Avatars(client);
+export const databases = new Databases(client);
+export const storage = new Storage(client);
